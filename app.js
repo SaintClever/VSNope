713a32fcd4c32eb56cbe95e7f6f8bfd9f4ee0c69
Saint. Clever <parchment@gmx.com>
2024-10-19 14:16:26 -0400

    update div width and create prevent default functionality for tab key

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 let textarea = document.querySelector("#textarea");
 let output = document.querySelector("#output");
+let snippet = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+</body>
+</html>`
 
 
-// textarea.focus();
+// focus
+setTimeout(() => {textarea.focus();}, 1000);
 
 
+// Code snippets
 let updateOuput = () => {
-  console.log(textarea.value);
+  if (textarea.value === "!!!") {
+    textarea.value = snippet;
+  }
   output.innerHTML = textarea.value;
 }
 
-textarea.addEventListener('keypress', () => {updateOuput()})
+// Prevent Default Tab
+let preventTab = (e) => {
+  if (e.key === "Tab") {
+    e.preventDefault();
+    textarea.value += "   ";
+  }
+}
+
+
+textarea.addEventListener("keyup", () => {updateOuput()})
+textarea.addEventListener("keydown", (e) => {preventTab(e)});
