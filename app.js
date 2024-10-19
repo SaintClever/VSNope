68de6e3c2a792dbfa8778eadf3e56b08f9bd61c5
Saint. Clever <parchment@gmx.com>
2024-10-19 13:38:43 -0400

    whimsical placeholder added

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ let textarea = document.querySelector("#textarea");
 let output = document.querySelector("#output");
 
 
+// textarea.focus();
+
+
 let updateOuput = () => {
-  // textarea.focus();
   console.log(textarea.value);
   output.innerHTML = textarea.value;
 }
-
 
 textarea.addEventListener('keypress', () => {updateOuput()})
