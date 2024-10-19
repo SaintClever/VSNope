39dcfb9737ced3d3954c38e9ca4431810c66c6d1
Saint. Clever <parchment@gmx.com>
2024-10-19 13:10:56 -0400

    Create updateOutput function to make it modular

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 let textarea = document.querySelector("#textarea");
 let output = document.querySelector("#output");
-// textarea.focus();
 
-textarea.addEventListener('keypress', () => {
+
+let updateOuput = () => {
+  // textarea.focus();
   console.log(textarea.value);
   output.innerHTML = textarea.value;
-})
+}
+
+
+textarea.addEventListener('keypress', () => {updateOuput()})
