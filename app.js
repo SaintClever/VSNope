6ac0dd99a7fde3d9d6e03cf0ca11d02c978e779e
Saint. Clever <parchment@gmx.com>
2024-10-20 09:45:47 -0400

    Update UI and and more JS commands

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 let textarea = document.querySelector("#textarea");
 let output = document.querySelector("#output");
-let snippet = `
-<!DOCTYPE html>
+let snippet = `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
 </head>
-
 <body>
-</body>
-</html>`
 
+</body>
+</html>`;
 
 // focus
 setTimeout(() => {textarea.focus();}, 1000);
@@ -20,8 +18,21 @@ setTimeout(() => {textarea.focus();}, 1000);
 
 // Code snippets
 let updateOuput = () => {
-  if (textarea.value === "!!!") {
+  if (textarea.value === "nope:!") {
     textarea.value = snippet;
+  }
+
+  if (textarea.value === "nope:css") {
+    textarea.value = '<link rel="stylesheet" href="style.css">';
+  }
+
+  if (textarea.value === "nope:dead") {
+    textarea.value = `
+    <h2>Bye Bye Bye Codesandbox and VScode</h2>
+    <video controls autoplay width="50%">
+    <source src="assets/Deadpool and Wolverine - Bye Bye Bye.mp4" type="video/mp4">
+    </video>
+    `
   }
   output.innerHTML = textarea.value;
 }
@@ -34,6 +45,10 @@ let preventTab = (e) => {
   }
 }
 
+
+let saveFile = () => {
+
+}
 
 textarea.addEventListener("keyup", () => {updateOuput()})
 textarea.addEventListener("keydown", (e) => {preventTab(e)});
